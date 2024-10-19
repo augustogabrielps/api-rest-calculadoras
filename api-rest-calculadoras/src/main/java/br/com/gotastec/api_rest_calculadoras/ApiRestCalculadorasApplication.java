25e9f4d594d3c7f83496a36f9f76bb0aad323a88
Augusto <deuvoid@gmail.com>
2024-10-19 20:23:12 -0300

    creating API project

---
+++ api-rest-calculadoras/src/main/java/br/com/gotastec/api_rest_calculadoras/ApiRestCalculadorasApplication.java
@@ -0,0 +1,13 @@
+package br.com.gotastec.api_rest_calculadoras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestCalculadorasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestCalculadorasApplication.class, args);
+	}
+
+}
