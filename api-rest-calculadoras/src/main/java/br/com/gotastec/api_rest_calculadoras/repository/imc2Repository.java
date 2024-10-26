d5da29109d6f6f0e595c8f7cc103af5cbe20e55e
Augusto <deuvoid@gmail.com>
2024-10-26 17:04:05 -0300

    creating methods to controllerClass

---
+++ api-rest-calculadoras/src/main/java/br/com/gotastec/api_rest_calculadoras/repository/imc2Repository.java
@@ -0,0 +1,10 @@
+package br.com.gotastec.api_rest_calculadoras.repository;
+
+import br.com.gotastec.api_rest_calculadoras.model.imc2;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface imc2Repository extends JpaRepository <imc2, Integer>{
+
+}
